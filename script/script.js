fb68709e680d5da81154899e527c402a193ed877
Nizrum <62435919+Nizrum@users.noreply.github.com>
2020-05-16 22:02:55 +0400

    Добавлено бургер-меню
    Добавлено бургер-меню

--- script/script.js
+++ script/script.js
@@ -3,13 +3,37 @@
 document.addEventListener('DOMContentLoaded', function () {
     const btnOpenModal = document.querySelector('#btnOpenModal');
     const modalBlock = document.querySelector('#modalBlock');
+    const modalWrap = document.querySelector('.modal');
     const closeModal = document.querySelector('#closeModal');
     const questionTitle = document.querySelector('#question');
     const formAnswers = document.querySelector('#formAnswers');
-    let burgerName1 = 'Стандарт';
-    let burgerName2 = 'Черный';
-    let burgerImg1 = './image/burger.png';
-    let burgerImg2 = './image/burgerBlack.png';
+    const burgerBtn = document.getElementById('burger');
+
+    burgerBtn.style.display = 'none';
+    
+    let clientWidth = document.documentElement.clientWidth;
+
+    if (clientWidth < 768) {
+        burgerBtn.style.display = 'flex';
+    } else {
+        burgerBtn.style.display = 'none';
+    }
+
+    window.addEventListener('resize', function () {
+        clientWidth = document.documentElement.clientWidth;
+
+        if (clientWidth < 768) {
+            burgerBtn.style.display = 'flex';
+        } else {
+            burgerBtn.style.display = 'none';
+        }
+    });
+
+    burgerBtn.addEventListener('click', () => {
+        burgerBtn.classList.add('active');
+        modalBlock.classList.add('d-block');
+        playTest();
+    });
 
     btnOpenModal.addEventListener('click', () => {
         modalBlock.classList.add('d-block');
@@ -18,11 +42,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
     closeModal.addEventListener('click', () => {
         modalBlock.classList.remove('d-block');
+        burgerBtn.classList.remove('active');
+    });
+
+    document.addEventListener('click', function (event) {
+        let target = event.target;
+        if (
+            !event.target.closest('.modal-dialog') &&
+            !event.target.closest('#btnOpenModal') &&
+            !event.target.closest('#burger')
+        ) {
+            modalBlock.classList.remove('d-block');
+            burgerBtn.classList.remove('active');
+        }
     });
 
     const playTest = () => {
         const renderQuestions = () => {
             questionTitle.textContent = 'Какого цвета бургер вы хотите?';
+
+            let burgerName1 = 'Стандарт';
+            let burgerName2 = 'Черный';
+            let burgerImg1 = './image/burger.png';
+            let burgerImg2 = './image/burgerBlack.png';
+            
             formAnswers.innerHTML = `
             <div class="answers-item d-flex flex-column">
                 <input type="radio" id="answerItem1" name="answer" class="d-none">
