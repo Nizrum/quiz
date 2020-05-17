812319aaff01cbccd2bcc204b44a2d0fedde4984
Nizrum <62435919+Nizrum@users.noreply.github.com>
2020-05-17 20:25:16 +0400

    Добавлено динамическое отображение вопросов
    Добавлено динамическое отображение вопросов и скрытие/появление кнопок prev/next в зависимости от номера вопроса

--- script/script.js
+++ script/script.js
@@ -8,6 +8,83 @@ document.addEventListener('DOMContentLoaded', function () {
     const questionTitle = document.querySelector('#question');
     const formAnswers = document.querySelector('#formAnswers');
     const burgerBtn = document.getElementById('burger');
+    const nextButton = document.querySelector('#next');
+    const prevButton = document.querySelector('#prev');
+
+    const questions = [
+        {
+            question: "Какого цвета бургер?",
+            answers: [
+                {
+                    title: 'Стандарт',
+                    url: './image/burger.png'
+                },
+                {
+                    title: 'Черный',
+                    url: './image/burgerBlack.png'
+                }
+            ],
+            type: 'radio'
+        },
+        {
+            question: "Из какого мяса котлета?",
+            answers: [
+                {
+                    title: 'Курица',
+                    url: './image/chickenMeat.png'
+                },
+                {
+                    title: 'Говядина',
+                    url: './image/beefMeat.png'
+                },
+                {
+                    title: 'Свинина',
+                    url: './image/porkMeat.png'
+                }
+            ],
+            type: 'radio'
+        },
+        {
+            question: "Дополнительные ингредиенты?",
+            answers: [
+                {
+                    title: 'Помидор',
+                    url: './image/tomato.png'
+                },
+                {
+                    title: 'Огурец',
+                    url: './image/cucumber.png'
+                },
+                {
+                    title: 'Салат',
+                    url: './image/salad.png'
+                },
+                {
+                    title: 'Лук',
+                    url: './image/onion.png'
+                }
+            ],
+            type: 'checkbox'
+        },
+        {
+            question: "Добавить соус?",
+            answers: [
+                {
+                    title: 'Чесночный',
+                    url: './image/sauce1.png'
+                },
+                {
+                    title: 'Томатный',
+                    url: './image/sauce2.png'
+                },
+                {
+                    title: 'Горчичный',
+                    url: './image/sauce3.png'
+                }
+            ],
+            type: 'radio'
+        }
+    ];
 
     burgerBtn.style.display = 'none';
     
@@ -58,31 +135,53 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     const playTest = () => {
-        const renderQuestions = () => {
-            questionTitle.textContent = 'Какого цвета бургер вы хотите?';
+        let numberQuestion = 0;
 
-            let burgerName1 = 'Стандарт';
-            let burgerName2 = 'Черный';
-            let burgerImg1 = './image/burger.png';
-            let burgerImg2 = './image/burgerBlack.png';
-            
-            formAnswers.innerHTML = `
-            <div class="answers-item d-flex flex-column">
-                <input type="radio" id="answerItem1" name="answer" class="d-none">
-                <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="${burgerImg1}" alt="burger">
-                    <span>${burgerName1}</span>
-                </label>
-            </div>
-            <div class="answers-item d-flex justify-content-center">
-                <input type="radio" id="answerItem2" name="answer" class="d-none">
-                <label for="answerItem2" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="${burgerImg2}" alt="burger">
-                    <span>${burgerName2}</span>
-                </label>
-            </div>
-            `;
+        const renderAnswers = (index) => {
+            questions[index].answers.forEach((answer) => {
+                const answerItem = document.createElement('div');
+                
+                answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
+                
+                answerItem.innerHTML = `
+                    <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+                    <label for="${answer.title}" class="d-flex flex-column justify-content-between">
+                        <img class="answerImg" src="${answer.url}" alt="burger">
+                        <span>${answer.title}</span>
+                    </label>
+                `;
+
+                formAnswers.appendChild(answerItem);
+            });
         }
-        renderQuestions();
+        const renderQuestions = (indexQuestion) => {
+            if (numberQuestion === 0) {
+                prevButton.style.display = 'none';
+            } else {
+                prevButton.style.display = 'block';
+            }
+            if (numberQuestion === questions.length - 1) {
+                nextButton.style.display = 'none';
+            } else {
+                nextButton.style.display = 'block';
+            }
+
+            formAnswers.innerHTML = '';
+
+            questionTitle.textContent = `${questions[indexQuestion].question}`;
+
+            renderAnswers(indexQuestion);
+        }
+        renderQuestions(numberQuestion);
+
+        nextButton.onclick =  () => {
+            numberQuestion++;
+            renderQuestions(numberQuestion);
+        };
+        
+        prevButton.onclick =  () => {
+            numberQuestion--;
+            renderQuestions(numberQuestion);
+        };
     }
 });
